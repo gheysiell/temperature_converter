d98788e95241c8b3205ae492bc22e26a2c52261f
gheysiell <gheysiellfarukzinn@hotmail.com>
2025-03-27 00:15:12 -0400

    remove double lib importation in MainActivity

--- app/src/main/java/com/example/temperature_converter/MainActivity.kt
+++ app/src/main/java/com/example/temperature_converter/MainActivity.kt
@@ -13,7 +13,6 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
-import androidx.compose.material3.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
